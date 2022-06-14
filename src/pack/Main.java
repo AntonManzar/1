2cba028bcfe8ad9b76c2d6d8f9bb0a4ad7be8d96
Anton Manzar <antonmanzar@mail.ru>
2022-06-14 18:41:36 +0300

    wefwef

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -1,8 +1,10 @@
 package pack;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+        double x = s.nextDouble(), y = s.nextDouble();
+        System.out.println((y>=2*x&&y<=3*x&&x>=0&&y!=2*x&&y!=3*x) ? "Yes" : "No");
     }
 }
